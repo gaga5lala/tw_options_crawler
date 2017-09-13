affc3113f76ca10a8c638cda4ac508618c7c480c
gaga5lala <gaga5lala1013@gmail.com>
2017-09-13 12:38:23 +0800

    post

--- option_crawler.py
+++ option_crawler.py
@@ -7,7 +7,31 @@ print("crawler for Taiwan options")
 
 import requests
 from bs4 import BeautifulSoup
-req = requests.get(url)
+
+# TODO: flexible change datetime
+
+# FIXME: payload incorrect
+payload = {
+    "qtype": 3,
+    "commodity_id": "TXOa",
+    # commodity_id2
+    "market_code": 1,
+    # goday
+    "dateaddcnt": -1,
+    "DATA_DATE_Y": 2017,
+    "DATA_DATE_M": 9,
+    "DATA_DATE_D": 13,
+    "syear": 2017,
+    "smonth": 9,
+    "sday": 13,
+    "datestart": "2017/9/13",
+    "MarketCode": 1,
+    "commodity_idt": "TXO",
+    # commodity_id2t
+    # commodity_id2t2
+    }
+
+req = requests.post(url, data=payload)
 soup = BeautifulSoup(req.text, 'html.parser')
 
 # soup.find(class="table_c")
@@ -24,3 +48,5 @@ for row in rows[1:]:
     cols = row.find_all('td')
     print(cols[1].text + ", " + cols[2].text + ", " + cols[3].text + ", " + cols[7].text + ", " + cols[12].text)
     idx += 1
+
+# TODO: save data
